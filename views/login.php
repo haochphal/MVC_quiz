f87ba13facaf91ebee9dcc378429ee24c5fda2ff
Haoch.Phal <Haoch.Phal@PNC.LAN>
2019-03-26 12:24:25 +0700

    view

---
+++ views/login.php
@@ -0,0 +1,44 @@
+<div class="container">
+    <form method="post" action="index.php?action=loginValidation">
+        <div id="div_login">
+            <h1>Login</h1>
+            <div>
+                <input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username" />
+            </div>
+            <div>
+                <input type="password" class="textbox" id="txt_uname" name="txt_pwd" placeholder="Password"/>
+            </div>
+            <div>
+                <input type="submit" value="Submit" name="but_submit" id="but_submit" />
+                <a href="index.php?action=register"  class="btn btn-success">Register</a>
+            </div>
+        </div>
+    </form>
+</div>
+
+<?php
+//include "config.php";
+if(isset($_POST['but_submit'])){
+
+    $uname = mysqli_real_escape_string($con,$_POST['txt_uname']);
+    $password = mysqli_real_escape_string($con,$_POST['txt_pwd']);
+
+    if ($uname != "" && $password != ""){
+
+        $sql_query = "select count(*) as cntUser from user where
+        username='".$uname."' and password='".$password."'";
+        $result = mysqli_query($con,$sql_query);
+        $row = mysqli_fetch_array($result);
+
+        $count = $row['cntUser'];
+
+        if($count > 0){
+            $_SESSION['uname'] = $uname;
+            header('Location: view');
+        }else{
+            echo "Invalid username and password";
+        }
+
+    }
+
+}
